6b25eec11e74933a58e69363b05dda7852f9f769
elorapeter <florenceonyi09@gmail.com>
2024-11-04 20:00:13 +0000

    Nov 4, 2024, 9:00 PM

---
+++ .vscode/cli/servers/Stable-38c31bc77e0dd6ae88a4e9cc93428cc27a56ba40/server/bin/remote-cli/code.cmd
@@ -0,0 +1,5 @@
+@echo off
+setlocal
+set ROOT_DIR=%~dp0..\..
+call "%ROOT_DIR%\node.exe" "%ROOT_DIR%\out\server-cli.js" "code" "1.93.1" "38c31bc77e0dd6ae88a4e9cc93428cc27a56ba40" "code.cmd" %*
+endlocal
